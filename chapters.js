c40c19beeb5b93d4b61958e476fdfa888651cec2
YasithD <yasith.18@cse.mrt.ac.lk>
2022-03-29 11:15:02 +0530

    class name fixed

--- chapters.js
+++ chapters.js
@@ -4,10 +4,10 @@ const chapters = document.querySelectorAll(".chapter-item > div");
 chapters.forEach((chapter) => {
   chapter.addEventListener("click", () => {
     let className = chapter.parentElement.className;
-    if (className === "chapter-item") {
+    if (className === "chapter-item ") {
       chapter.parentElement.className = "chapter-item expanded";
     } else {
-      chapter.parentElement.className = "chapter-item";
+      chapter.parentElement.className = "chapter-item ";
     }
   });
 });
